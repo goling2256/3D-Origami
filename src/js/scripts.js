66fef30ad22e8860519d885a6f99d6d5b6126572
Gordon Ling <gordonling2256@gmail.com>
2023-08-17 01:24:42 -0700

    Setting up scene

---
+++ src/js/scripts.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.shadowMap.enabled = true;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+camera.position.z = 5;
+
+renderer.render(scene, camera);
